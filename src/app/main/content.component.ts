6bc9fd47c5220e6f6a5764df1b2ff6d3a9756fd6
Leonardo Viegas <leonardoviegas@gmail.com>
2020-03-17 15:55:06 +0000

    Added Sign Type Editing

--- src/app/main/content.component.ts
+++ src/app/main/content.component.ts
@@ -1,11 +1,68 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, TemplateRef } from "@angular/core";
 import { AuthService } from "./../auth/auth.service";
+import { TrafficSignsService } from "./traffic-signs.service";
+import { BsModalService, BsModalRef } from "ngx-bootstrap";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-content",
   templateUrl: "./content.component.html",
   styleUrls: ["./content.component.css"]
 })
-export class ContentComponent {
-  constructor(private authService: AuthService) {}
+export class ContentComponent implements OnInit {
+  constructor(
+    private trafficSignsService: TrafficSignsService,
+    private modalService: BsModalService
+  ) {}
+  title = "";
+  data = {};
+  modalRef: BsModalRef;
+  editSignTypeForm: FormGroup;
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    // this.form.patchValue({image: file});
+    // this.form.get('image').updateValueAndValidity();
+    // const reader = new FileReader();
+    // reader.onload = () => {
+    //   this.imagePreview = reader.result as string;
+    // };
+    // reader.readAsDataURL(file as Blob);
+  }
+
+  ngOnInit() {
+    this.editSignTypeForm = new FormGroup({
+      Name: new FormControl(null),
+      Text: new FormControl(null),
+      Placement_Text: new FormControl(null)
+    });
+  }
+
+  getSignType(signId: number) {
+    this.trafficSignsService.getSignType(signId, (err, res) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      this.data = res;
+      console.log(this.data);
+      this.title = res.Name;
+    });
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  editSignTypeF(data) {
+    var SignTypeFields = ["Name", "Text", "Placement_Text"];
+
+    SignTypeFields.forEach(signTypeField => {
+      if (this.data[signTypeField] === data[signTypeField])
+        delete data[signTypeField];
+    });
+    
+    this.trafficSignsService.patchSignType(this.data['idTraffic_Signs_Type'], data)
+    window.location.reload();
+  }
 }
--- src/app/main/traffic-signs.service.ts
+++ src/app/main/traffic-signs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "../../environments/environment";
-import { Subject } from 'rxjs';
+import { Subject } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class TrafficSignsService {
@@ -14,27 +14,70 @@ export class TrafficSignsService {
   goToSignType(idSignalType: number) {
     this.activeDiagram.next(false);
     this.http
-      .get(environment.apiUrl + "/content/traffic-signs/?idTraffic_Signs_Type=" + idSignalType, { headers: this.headers })
+      .get(
+        environment.apiUrl +
+          "/content/traffic-signs/?idTraffic_Signs_Type=" +
+          idSignalType,
+        { headers: this.headers }
+      )
       .subscribe(
         res => this.signalType.next(res),
         err => this.activeDiagram.next(true)
       );
   }
 
+  getSignType(idSignType: number, cb) {
+    this.http
+      .get(
+        environment.apiUrl +
+          "/content/traffic-signs/?idTraffic_Signs_Type=" +
+          idSignType,
+        { headers: this.headers }
+      )
+      .subscribe(
+        res => cb(false, res),
+        err => cb(err)
+      );
+  }
+
   getSignsImage(idSignalType: number, cb) {
     this.http
-    .get(environment.apiUrl + "/content/traffic-signs/images/?idTraffic_Signs_Type=" + idSignalType, { headers: this.headers })
-    .subscribe(
-      res => cb(false, res),
-      err => cb(err)
-    );
+      .get(
+        environment.apiUrl +
+          "/content/traffic-signs/images/?idTraffic_Signs_Type=" +
+          idSignalType,
+        { headers: this.headers }
+      )
+      .subscribe(
+        res => cb(false, res),
+        err => cb(err)
+      );
   }
   getSign(idTraffic_Signs: number, cb) {
     this.http
-    .get(environment.apiUrl + "/content/traffic-signs/sign/?idTraffic_Signs=" + idTraffic_Signs, { headers: this.headers })
-    .subscribe(
-      res => cb(false, res),
-      err => cb(err)
-    );
+      .get(
+        environment.apiUrl +
+          "/content/traffic-signs/sign/?idTraffic_Signs=" +
+          idTraffic_Signs,
+        { headers: this.headers }
+      )
+      .subscribe(
+        res => cb(false, res),
+        err => cb(err)
+      );
+  }
+  patchSignType(idTraffic_Signs_Type: number, data) {
+    this.http
+      .patch(
+        environment.apiUrl +
+          "/content/traffic-signs/?idTraffic_Signs_Type=" +
+          idTraffic_Signs_Type,
+        data,
+        { headers: this.headers }
+      )
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      );
   }
 }
